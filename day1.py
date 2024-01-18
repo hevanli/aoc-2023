1b21612a7d321b44dde46ab7035d8029b10c3351
hevanli <evanlicubs@gmail.com>
2024-01-18 13:51:47 -0800

    day 1 and day 2

---
+++ day1.py
@@ -0,0 +1,44 @@
+# take in file input
+filename = "input1.txt"
+lines = []
+with (open(filename)) as file:
+    lines = [line.strip() for line in file.readlines()]
+
+def part1():
+    sum = 0
+    for line in lines:
+        # create list of only the digits in the line
+        nums = [char for char in line if char.isdigit()] 
+        # combine first and last digit in list and add to sum
+        sum += (int)(nums[0] + nums[len(nums) - 1]) 
+    return sum
+
+def part2():
+    sum = 0
+    replace = {
+            "one": "1",
+            "two": "2",
+            "three": "3",
+            "four": "4",
+            "five": "5",
+            "six": "6",
+            "seven": "7",
+            "eight": "8",
+            "nine": "9"
+            }
+    for line in lines:
+        digits = []
+        # traverse char by char, rewriting the line
+        for i, c in enumerate(line):
+            if (c.isdigit()):
+                digits.append(c)
+            else:
+                for k in replace.keys():
+                    if (line[i:].startswith(k)):
+                        digits.append(replace[k])
+                        break
+        sum += (int)(digits[0] + digits[len(digits) - 1])
+    return sum
+
+print(part1())
+print(part2())
---
+++ day2.py
@@ -0,0 +1,60 @@
+filename = "input2.txt"
+lines = []
+with (open(filename)) as file:
+    lines = [line.strip() for line in file.readlines()]
+
+def part1():
+    sum = 0
+    # iterates through each game
+    for i, line in enumerate(lines):
+        flag = True
+        subsets = line[line.index(":")+2:].split(";")
+        # iterates through each subset in each game
+        for subset in subsets:
+            colors = [color.strip() for color in subset.split(", ")]
+            r,g,b = 0,0,0
+            for color in colors:
+                indexOfSpace = color.index(" ")
+                if ("red" in color):
+                    r = (int)(color[0:indexOfSpace])
+                if ("green" in color):
+                    g = (int)(color[0:indexOfSpace])
+                if ("blue" in color):
+                    b = (int)(color[0:indexOfSpace])
+                if (r > 12 or g > 13 or b > 14):
+                    flag = False
+                    break
+        if flag:
+            sum += i + 1
+
+    return sum
+
+def part2():
+    sum = 0
+    # iterates through each game
+    for i, line in enumerate(lines):
+        g_r,g_g,g_b = 0,0,0
+        subsets = line[line.index(":")+2:].split(";")
+        # iterates through each subset in each game
+        for subset in subsets:
+            colors = [color.strip() for color in subset.split(", ")]
+            r,g,b = 0,0,0
+            for color in colors:
+                indexOfSpace = color.index(" ")
+                if ("red" in color):
+                    r = (int)(color[0:indexOfSpace])
+                if ("green" in color):
+                    g = (int)(color[0:indexOfSpace])
+                if ("blue" in color):
+                    b = (int)(color[0:indexOfSpace])
+            g_r = max(g_r, r)
+            g_g = max(g_g, g)
+            g_b = max(g_b, b)
+        sum += g_r * g_g * g_b
+
+    return sum
+
+
+print(part1())
+print(part2())
+
