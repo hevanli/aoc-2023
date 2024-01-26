5c521eed50de895a0668ce1f92db6cbb4c41e90e
hevanli <evanlicubs@gmail.com>
2024-01-25 16:58:36 -0800

    day 8 part 1

---
+++ day8/day8.py
@@ -0,0 +1,27 @@
+filename = "input.txt"
+lines = []
+with (open(filename)) as file:
+    lines = [line.strip() for line in file.readlines()]
+
+def part1():
+    moves = lines[0]
+    node_network = {}
+    for line in lines[2:]:
+        parts = line.split()
+        key = parts[0]
+        node = (parts[2][1:len(parts[2])-1], parts[3][:len(parts[3])-1])
+        node_network.update({key: node})
+
+    steps = 0
+    cur = "AAA"
+    while cur != "ZZZ":
+        for step in moves:
+            cur = node_network[cur][0 if step == "L" else 1]
+            steps += 1
+            if cur == "ZZZ": 
+                break
+
+    print(steps)
+
+part1()
+            
