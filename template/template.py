433a9ea787e8c312e79de0d6aa4a1e537a04c69d
hevanli <evanlicubs@gmail.com>
2024-02-05 10:35:19 -0800

    template adjusting

--- template/template.py
+++ template/template.py
@@ -1,12 +1,18 @@
-filename = "testInput.txt"
-lines = []
-with (open(filename)) as file:
-    lines = [line.strip() for line in file.readlines()]
+def parse(filename: str) -> list[str]:
+    lines = []
+    with open(filename) as f:
+        lines = [line.strip() for line in f.readlines()]
+    return lines
 
-def part1():
+def part1(filename: str) -> None:
+    lines = parse(filename)
     return
 
-def part2():
+def part2(filename: str) -> None:
+    lines = parse(filename)
     return
 
-part1()
+print("--test--")
+part1("testInput.txt")
+print("--real--")
+part2("input.txt")
